5136e8cd48aef6e41785e59992e0f3c427d9a6d1
Xavier JULIEN <xjulien@molotov.tv>
2022-07-15 14:48:22 +0200

    Feat: app size depends of viewport size

---
+++ src/helpers/sizes.js
@@ -0,0 +1,7 @@
+export const totalWidth = window.visualViewport.width
+export const totalHeight = window.visualViewport.height
+
+export default {
+  totalWidth,
+  totalHeight,
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { Launch } from '@lightningjs/sdk'
 import App from './App.js'
+import sizes from './helpers/sizes.js'
 
 export default function() {
-  return Launch(App, ...arguments)
+  const options = arguments
+
+  options[0].stage.w = sizes.totalWidth
+  options[0].stage.h = sizes.totalHeight
+
+  return Launch(App, ...options)
 }
